a799e408f9cfc6d42d02d52ad662e86686b83664
Md. Rony <ronimdrahim467@gmail.com>
2025-10-22 00:05:07 +0600

    sing in sing out

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,77 @@
 import "./App.css";
-import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import {
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
 import { auth } from "./firebase.init";
 import { useState } from "react";
 
 const provider = new GoogleAuthProvider();
+const githubProvider = new GithubAuthProvider();
 
 function App() {
-  const [user,setUser]= useState(null);
+  const [user, setUser] = useState(null);
 
   const handleSingIn = () => {
     signInWithPopup(auth, provider)
       .then((result) => {
         console.log(result.user);
-        setUser(result.user)
+        setUser(result.user);
       })
       .catch((error) => {
         console.log(error);
       });
   };
-  const handleSingOut=()=>{
-    signOut(auth)
-    .then(()=>{
-      setUser(null)
+  const handleGithub = () => {
+    signInWithPopup(auth, githubProvider)
+      .then((result) => {
+        console.log(result.user);
+        setUser(result.user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+ const handleSingOut = () => {
+  signOut(auth)
+    .then(() => {
+      setUser(null);
     })
-    .then(error=>{
-      console.log(error)
-    })
-  }
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+
 
   return (
     <>
       <h1>Google Sing-in Sing-out</h1>
-     
-     
-      {user? <button className="btn" onClick={handleSingOut}>Sing Out</button>: <button className="btn" onClick={handleSingIn}>Google Sing in</button> }
-      {
-        user && <div>
-        <p>{user.displayName}</p>
-        <p>{user.email}</p>
-        <img src={user.imageURL} alt="" />
-      </div>
-      }
+
+      {user ? (
+        <button className="btn" onClick={handleSingOut}> Sign Out</button>
+       ) : (
+        <>
+          <button className="btn" onClick={handleSingIn}>
+            Google Sign In
+          </button>
+          <br />
+          <button className="btn" onClick={handleGithub}>
+            Github Sign In
+          </button>
+        </>
+      )}
+
+      {user && (
+        <div>
+          <p>{user.displayName}</p>
+          <p>{user.email}</p>
+          <img src={user.photoURL} alt="" />
+        </div>
+      )}
     </>
   );
 }
